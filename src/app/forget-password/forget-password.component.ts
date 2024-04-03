efd6ecbe65644d40185c5dd354f2a67c2b8f8fb7
bhandekunal16 <bhandekunal16@gmail.com>
2024-04-03 16:30:44 +0530

    "Add forget password form with Primeng messages and reactive forms"
    This diff adds a new forget password form to the forget-password component. It includes a Primeng message component to display messages to the user and uses reactive forms for form handling. The form includes a single input field for the user's mobile number. The component's constructor creates a new reactive form with a single form control for the mobile number input. The component's template and styles have also been updated to include the new form and message components.

--- src/app/forget-password/forget-password.component.ts
+++ src/app/forget-password/forget-password.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { MessagesModule } from 'primeng/messages';
+import { Message } from 'primeng/api';
+import { FormGroup, FormControl } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Router } from 'express';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-forget-password',
   standalone: true,
-  imports: [],
+  imports: [MessagesModule, CommonModule, ReactiveFormsModule],
   templateUrl: './forget-password.component.html',
-  styleUrl: './forget-password.component.scss'
+  styleUrl: './forget-password.component.scss',
 })
 export class ForgetPasswordComponent {
+  public msg: Message[] | any;
+  public myForm: FormGroup;
 
+  constructor(private router: Router) {
+    this.myForm = new FormGroup({
+      mobileNo: new FormControl(''),
+    });
+  }
 }
