62730656bf63239bca4cab07d5f9e7ccebd4eb00
bhandekunal16 <bhandekunal16@gmail.com>
2024-04-04 10:13:12 +0530

    Update conform-password component with form and validation; add password strength pattern
    The conform-password component has been updated with a form and validation for the new password field. A password strength pattern has been added to ensure the new password meets certain complexity requirements. The component also now displays a success message when the password is changed successfully and navigates to the dashboard upon successful password change.
    
    Additionally, the component's template and styles have been updated to improve the user interface and experience. The component is now standalone and imports necessary modules from Angular and PrimeNG.
    
    The verify-otp component's styles have also been updated to improve the user interface and experience on smaller screens.

--- src/app/conform-password/conform-password.component.ts
+++ src/app/conform-password/conform-password.component.ts
@@ -1,12 +1,59 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
+import { MessagesModule } from 'primeng/messages';
+import { Message } from 'primeng/api';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-conform-password',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ButtonModule, MessagesModule, ReactiveFormsModule],
   templateUrl: './conform-password.component.html',
-  styleUrl: './conform-password.component.scss'
+  styleUrl: './conform-password.component.scss',
 })
 export class ConformPasswordComponent {
+  public myForm: FormGroup;
+  public msg: Message[] | any;
 
+  constructor(private router: Router) {
+    this.myForm = new FormGroup({
+      Password: new FormControl('', [
+        Validators.required,
+        Validators.pattern(
+          /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
+        ),
+      ]),
+    });
+  }
+
+  submitForm() {
+    const password = this.myForm.value.password;
+
+    if (password !== '') {
+      this.msg = [
+        {
+          severity: 'success',
+          summary: 'success',
+          detail: 'password changed successfully.',
+        },
+      ];
+
+      this.dashboard();
+    } else {
+      this.msg = [
+        {
+          severity: 'warn',
+          summary: 'warn',
+          detail: 'something went wrong',
+        },
+      ];
+    }
+  }
+
+  dashboard(): void {
+    this.router.navigate(['/dashboard']);
+  }
 }
