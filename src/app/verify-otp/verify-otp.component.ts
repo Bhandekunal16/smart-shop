bf1210c79450ad0f48589161027aed43db0a3d79
bhandekunal16 <bhandekunal16@gmail.com>
2024-04-04 09:31:36 +0530

    "Add form control for OTP input and messages component for displaying success/warn messages in VerifyOtpComponent"
    This diff adds form control for OTP input and messages component for displaying success/warn messages in VerifyOtpComponent. The form control is added using reactive forms and the messages component is imported from primeng. The component also includes validation for the OTP input field to ensure that it is a 4-digit number. The submitForm() method has been updated to check if the entered OTP is correct and display the appropriate message.

--- src/app/verify-otp/verify-otp.component.ts
+++ src/app/verify-otp/verify-otp.component.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
+import { MessagesModule } from 'primeng/messages';
+import { Message } from 'primeng/api';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-verify-otp',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, CommonModule, MessagesModule, ReactiveFormsModule],
   templateUrl: './verify-otp.component.html',
-  styleUrl: './verify-otp.component.scss'
+  styleUrl: './verify-otp.component.scss',
 })
 export class VerifyOtpComponent {
+  public msg: Message[] | any;
+  public myForm: FormGroup;
 
+  constructor(private router: Router, private formBuilder: FormBuilder) {
+    this.myForm = this.formBuilder.group({
+      otp: ['', [Validators.required, Validators.pattern(/^\d{4}$/)]]
+    });
+  }
+
+  submitForm() {
+    const otp: string = this.myForm.value.otp;
+
+    console.log(otp)
+
+    if (otp == '0000') {
+      this.msg = [
+        {
+          severity: 'success',
+          summary: 'success',
+          detail: 'otp-verified',
+        },
+      ];
+    } else {
+      this.msg = [
+        {
+          severity: 'warn',
+          summary: 'warn',
+          detail: 'check again',
+        },
+      ];
+    }
+  }
 }
