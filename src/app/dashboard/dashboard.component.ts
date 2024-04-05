f6ddbdaa404f36fad39327bc6500dd38e4eb0030
bhandekunal16 <bhandekunal16@gmail.com>
2024-04-05 11:57:21 +0530

    Add primeng menubar to dashboard component and initialize with menu items
    This diff adds the primeng MenubarModule to the imports of the DashboardComponent and initializes it with a menu model consisting of several MenuItem objects. The menu model contains a hierarchical structure of menu items, each with a label, icon, and potentially child menu items.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,108 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MenubarModule, CommonModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
+  items: MenuItem[] | undefined;
 
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Shop',
+        icon: 'pi pi-fw pi-file',
+        items: [
+          {
+            label: 'Information',
+            icon: 'pi pi-fw pi-plus',
+            items: [
+              {
+                label: 'Edit',
+                icon: 'pi pi-fw pi-bookmark',
+              },
+              {
+                label: 'View',
+                icon: 'pi pi-fw pi-video',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        label: 'Users',
+        icon: 'pi pi-fw pi-user',
+        items: [
+          {
+            label: 'New',
+            icon: 'pi pi-fw pi-user-plus',
+          },
+          {
+            label: 'Delete',
+            icon: 'pi pi-fw pi-user-minus',
+          },
+          {
+            label: 'Search',
+            icon: 'pi pi-fw pi-users',
+            items: [
+              {
+                label: 'Filter',
+                icon: 'pi pi-fw pi-filter',
+                items: [
+                  {
+                    label: 'Print',
+                    icon: 'pi pi-fw pi-print',
+                  },
+                ],
+              },
+              {
+                icon: 'pi pi-fw pi-bars',
+                label: 'List',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        label: 'Product',
+        icon: 'pi pi-fw pi-calendar',
+        items: [
+          {
+            label: 'Operation',
+            icon: 'pi pi-fw pi-pencil',
+            items: [
+              {
+                label: 'Edit',
+                icon: 'pi pi-fw pi-calendar-plus',
+              },
+              {
+                label: 'Delete',
+                icon: 'pi pi-fw pi-calendar-minus',
+              },
+            ],
+          },
+          {
+            label: 'Upload',
+            icon: 'pi pi-fw pi-calendar-times',
+            items: [
+              {
+                label: 'New',
+                icon: 'pi pi-fw pi-calendar-minus',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        label: 'Quit',
+        icon: 'pi pi-fw pi-power-off',
+      },
+    ];
+  }
 }
