cca5eafb114c73a1415cacdcad63ff6f50c44dfb
bhandekunal16 <bhandekunal16@gmail.com>
2024-04-05 09:04:17 +0530

    "Add forget password functionality using HttpClient and Observable"
    This diff adds the ability to send an OTP to a user's email address for password recovery. It introduces the use of HttpClient and Observable to make HTTP requests to a server. The `sendOtp` method is added to the component, which sends a POST request to the server with the user's email address. If the request is successful, the OTP is sent and the user is navigated to the OTP verification page. If the request fails, an error message is displayed. The `HttpClientModule` is also imported to enable the use of HttpClient.

--- src/app/forget-password/forget-password.component.ts
+++ src/app/forget-password/forget-password.component.ts
@@ -8,10 +8,23 @@ import { MessagesModule } from 'primeng/messages';
 import { Message } from 'primeng/api';
 import { SettlerService } from '../common/settler.service';
 
+import { Observable, catchError, throwError } from 'rxjs';
+import {
+  HttpClient,
+  HttpClientModule,
+  HttpHeaders,
+} from '@angular/common/http';
+
 @Component({
   selector: 'app-forget-password',
   standalone: true,
-  imports: [ButtonModule, ReactiveFormsModule, CommonModule, MessagesModule],
+  imports: [
+    ButtonModule,
+    ReactiveFormsModule,
+    CommonModule,
+    MessagesModule,
+    HttpClientModule,
+  ],
   templateUrl: './forget-password.component.html',
   styleUrl: './forget-password.component.scss',
 })
@@ -19,37 +32,57 @@ export class ForgetPasswordComponent {
   public myForm: FormGroup;
   public msg: Message[] | any;
 
-  constructor(private router: Router, private settler: SettlerService) {
+  constructor(
+    private router: Router,
+    private settler: SettlerService,
+    private http: HttpClient
+  ) {
     this.myForm = new FormGroup({
       email: new FormControl(''),
     });
   }
 
   submitForm() {
-    const gmail: string = this.myForm.value.email;
-    this.settler.emailObj=gmail
+    const email: string = this.myForm.value.email;
+    this.settler.emailObj = email;
 
-    if (gmail == '') {
-      this.msg = [
-        {
-          severity: 'warn',
-          summary: 'warn',
-          detail: 'something went wrong',
-        },
-      ];
-    } else {
-      this.verifyOtp();
-      this.msg = [
-        {
-          severity: 'success',
-          summary: 'Success',
-          detail: 'otp send to your email.',
-        },
-      ];
-    }
+    this.sendOtp({ email }).subscribe((data) => {
+      if (!data.status) {
+        this.msg = [
+          {
+            severity: 'warn',
+            summary: 'warn',
+            detail: 'something went wrong',
+          },
+        ];
+      } else {
+        this.verifyOtp();
+        this.msg = [
+          {
+            severity: 'success',
+            summary: 'Success',
+            detail: 'otp send to your email.',
+          },
+        ];
+      }
+    });
   }
 
   verifyOtp(): void {
     this.router.navigate(['/verify-otp']);
+  }
+
+  sendOtp(body: any): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    return this.http
+      .post<any>('http://localhost:3003/auth/otp/send', body, { headers })
+      .pipe(
+        catchError((error) => {
+          return throwError(error);
+        })
+      );
   }
 }
