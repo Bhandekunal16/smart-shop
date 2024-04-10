609ca9acbb706e4a23f9ea5701fa983678265853
bhandekunal16 <bhandekunal16@gmail.com>
2024-04-10 10:07:29 +0530

    "Add form inputs for shop details, update HTML and SCSS, import ButtonModule from primeng"
    This commit includes the following changes:
    
    * Added form inputs for shop name, address, official email, official contact number, shop GST, PAN card, and pin code in the add-shop.component.html file.
    * Updated the HTML and SCSS files to style the new form inputs and add a tip section.
    * Imported the ButtonModule from primeng in the add-shop.component.ts file.
    
    The commit message is concise and describes the changes made in the code diff without making any interpretations.

--- src/app/add-shop/add-shop.component.ts
+++ src/app/add-shop/add-shop.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-add-shop',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './add-shop.component.html',
-  styleUrl: './add-shop.component.scss'
+  styleUrl: './add-shop.component.scss',
 })
 export class AddShopComponent {
-
+  submitForm() {}
 }
