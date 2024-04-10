e80e485b110ea8f7190ee66dd482de718b8b11df
bhandekunal16 <bhandekunal16@gmail.com>
2024-04-10 10:23:04 +0530

    Add reactive form validation to add shop component
    The changes include:
    - Importing necessary modules for reactive forms
    - Creating a new FormGroup with form controls and validation for each input field in the template
    - Adding *ngIf directives to display error messages for invalid inputs
    - Updating the form selector to include formGroup attribute for reactive forms

--- src/app/add-shop/add-shop.component.ts
+++ src/app/add-shop/add-shop.component.ts
@@ -1,13 +1,46 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-add-shop',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, ReactiveFormsModule, CommonModule],
   templateUrl: './add-shop.component.html',
   styleUrl: './add-shop.component.scss',
 })
 export class AddShopComponent {
+  public myForm: FormGroup | any;
+
+  constructor() {
+    this.myForm = new FormGroup({
+      ShopName: new FormControl('', Validators.required),
+      Address: new FormControl('', Validators.required),
+      officialEmail: new FormControl('', [
+        Validators.required,
+        Validators.email,
+      ]),
+      officialContactNo: new FormControl('', [
+        Validators.required,
+        Validators.pattern('[0-9]{10}'),
+      ]),
+      shopGst: new FormControl('', Validators.pattern('[0-9]{15}')),
+      panNo: new FormControl(
+        '',
+        Validators.pattern('[A-Z]{5}[0-9]{4}[A-Z]{1}')
+      ),
+      pinCode: new FormControl('', [
+        Validators.required,
+        Validators.pattern('[0-9]{6}'),
+      ]),
+    });
+  }
+
   submitForm() {}
 }
