6f6798474c8f06c0402db042ef6e5c22838d2b03
bhandekunal16 <bhandekunal16@gmail.com>
2024-04-12 10:00:31 +0530

    "Added view-shop component with API call to fetch shop details"
    This diff includes the following changes:
    
    * The `view-shop.component.html` file has been updated to include a PrimeNG card with placeholders for

--- src/app/view-shop/view-shop.component.ts
+++ src/app/view-shop/view-shop.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { CommonModule } from '@angular/common';
+import {
+  HttpClient,
+  HttpClientModule,
+  HttpHeaders,
+} from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-view-shop',
   standalone: true,
-  imports: [],
+  imports: [CardModule, HttpClientModule, CommonModule],
   templateUrl: './view-shop.component.html',
-  styleUrl: './view-shop.component.scss'
+  styleUrl: './view-shop.component.scss',
 })
-export class ViewShopComponent {
+export class ViewShopComponent implements OnInit {
+  constructor(private http: HttpClient) {}
 
+  public shopName: string | undefined;
+  public shopAddress: string | undefined;
+  public mobileNumber: string | undefined;
+  public email: string | undefined;
+
+  ngOnInit(): void {
+    this.details();
+  }
+
+  details() {
+    const id = localStorage.getItem('id');
+
+    this.shopDetails({ id }).subscribe((ele) => {
+      console.log(ele);
+      this.shopName = ele.data.shopName;
+      this.shopAddress = ele.data.address;
+      this.mobileNumber = ele.data.officialContactNo;
+      this.email = ele.data.officialEmail;
+      console.log(this.shopName);
+    });
+  }
+
+  shopDetails(body: any): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    return this.http
+      .post<any>('http://localhost:3003/shop/search', body, { headers })
+      .pipe(
+        catchError((error) => {
+          return throwError(error);
+        })
+      );
+  }
 }
