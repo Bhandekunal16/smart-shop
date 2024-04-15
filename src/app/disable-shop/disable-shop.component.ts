df0c7845bd644ba499cfd15b42d592bf4377b8e0
bhandekunal16 <bhandekunal16@gmail.com>
2024-04-15 11:28:54 +0530

    "Added primeng messages module and implemented trigger functions for activation and deactivation with success messages and reload"
    This diff has added the primeng messages module and its associated Message class to the imports of the disable-shop component. Two

--- src/app/disable-shop/disable-shop.component.ts
+++ src/app/disable-shop/disable-shop.component.ts
@@ -7,16 +7,19 @@ import {
 } from '@angular/common/http';
 import { Observable, catchError, throwError } from 'rxjs';
 import { ButtonModule } from 'primeng/button';
+import { MessagesModule } from 'primeng/messages';
+import { Message } from 'primeng/api';
 
 @Component({
   selector: 'app-disable-shop',
   standalone: true,
-  imports: [CommonModule, HttpClientModule, ButtonModule],
+  imports: [CommonModule, HttpClientModule, ButtonModule, MessagesModule],
   templateUrl: './disable-shop.component.html',
   styleUrl: './disable-shop.component.scss',
 })
 export class DisableShopComponent implements OnInit {
   public products: any[] = [];
+  public msg: Message[] | any;
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
@@ -27,7 +30,6 @@ export class DisableShopComponent implements OnInit {
     const id = localStorage.getItem('id');
     this.shopDetails({ id }).subscribe((ele) => {
       this.products.push(ele.data);
-      console.log(this.products);
     });
   }
 
@@ -38,6 +40,68 @@ export class DisableShopComponent implements OnInit {
 
     return this.http
       .post<any>('http://localhost:3003/shop/search', body, { headers })
+      .pipe(
+        catchError((error) => {
+          return throwError(error);
+        })
+      );
+  }
+
+  trigger() {
+    const id = localStorage.getItem('id');
+    this.activate({ id }).subscribe((ele) => {
+      this.msg = [
+        {
+          severity: 'success',
+          summary: 'Success',
+          detail: 'shop activated successfully!',
+        },
+      ];
+
+      setTimeout(() => {
+        window.location.reload();
+      }, 2000);
+    });
+  }
+
+  trigger2() {
+    const id = localStorage.getItem('id');
+    this.deactivated({ id }).subscribe((ele) => {
+      this.msg = [
+        {
+          severity: 'success',
+          summary: 'Success',
+          detail: 'shop Deactivated successfully!',
+        },
+      ];
+
+      setTimeout(() => {
+        window.location.reload();
+      }, 2000);
+    });
+  }
+
+  activate(body: any): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    return this.http
+      .post<any>('http://localhost:3003/shop/enable', body, { headers })
+      .pipe(
+        catchError((error) => {
+          return throwError(error);
+        })
+      );
+  }
+
+  deactivated(body: any): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    return this.http
+      .post<any>('http://localhost:3003/shop/disable', body, { headers })
       .pipe(
         catchError((error) => {
           return throwError(error);
