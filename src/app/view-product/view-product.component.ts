8ed8ed487b817d7bc5a2a473991b452be8271d59
bhandekunal16 <bhandekunal16@gmail.com>
2024-04-29 10:35:38 +0530

    "Add view-product component to display product details; fetch data via HTTP request"
    This diff includes the following changes:
    
    * The view-product component template has been updated to display product information in a PrimeNG card component.

--- src/app/view-product/view-product.component.ts
+++ src/app/view-product/view-product.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  HttpClient,
+  HttpClientModule,
+  HttpHeaders,
+} from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
+import { CardModule } from 'primeng/card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-view-product',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, CardModule, CommonModule],
   templateUrl: './view-product.component.html',
-  styleUrl: './view-product.component.scss'
+  styleUrl: './view-product.component.scss',
 })
-export class ViewProductComponent {
+export class ViewProductComponent implements OnInit {
+  public data: any[] = [];
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.shopDetails().subscribe((ele) => {
+      this.data = ele.data;
+      console.log(this.data);
+    });
+  }
 
+  shopDetails(): Observable<any> {
+    const id = localStorage.getItem('id');
+    console.log(id, 'i am hitting');
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+    return this.http
+      .get<any>(`http://localhost:3003/product/getall/${id}`, { headers })
+      .pipe(
+        catchError((error) => {
+          return throwError(error);
+        })
+      );
+  }
 }
