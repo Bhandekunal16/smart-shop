214d706c55f6d93d9416bb11ae185cccecc97c61
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-03 10:04:08 +0530

    "Add unsubscribe button to user list and related functionality"
    This diff adds a new button to the user list table that allows for unsubscribing a user. When the button is clicked, it calls the `unsubscribe()` method

--- src/app/userlist/userlist.component.ts
+++ src/app/userlist/userlist.component.ts
@@ -9,16 +9,25 @@ import { DecryptService } from '../../global/decrypt.service';
 import { TableModule } from 'primeng/table';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
+import { MessagesModule } from 'primeng/messages';
+import { Message } from 'primeng/api';
 
 @Component({
   selector: 'app-userlist',
   standalone: true,
-  imports: [HttpClientModule, TableModule, CommonModule, ButtonModule],
+  imports: [
+    HttpClientModule,
+    TableModule,
+    CommonModule,
+    ButtonModule,
+    MessagesModule,
+  ],
   templateUrl: './userlist.component.html',
   styleUrl: './userlist.component.scss',
 })
 export class UserlistComponent implements OnInit {
   products!: any[];
+  public msg: Message[] | any;
   constructor(private http: HttpClient, private decrypt: DecryptService) {}
 
   ngOnInit(): void {
@@ -27,6 +36,36 @@ export class UserlistComponent implements OnInit {
       this.products = data.data;
     });
   }
+
+  unsubscribe(id: any) {
+    this.customerUnsubscribed(id).subscribe((ele) => {
+      console.log(ele);
+      const data = this.decrypt.decrypt(ele.response);
+
+      if (data.status) {
+        this.msg = [
+          {
+            severity: 'success',
+            summary: 'success',
+            detail: `${data.data}`,
+          },
+        ];
+      } else {
+        this.msg = [
+          {
+            severity: 'warn',
+            summary: 'warn',
+            detail: 'some addition failed.',
+          },
+        ];
+      }
+
+      setTimeout(() => {
+        window.location.reload();
+      }, 2000);
+    });
+  }
+
   customerSubscribed(): Observable<any> {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
@@ -38,6 +77,20 @@ export class UserlistComponent implements OnInit {
         `http://localhost:3003/auth/getAll/customers/subscribed/${id}`,
         { headers }
       )
+      .pipe(
+        catchError((error) => {
+          return throwError(error);
+        })
+      );
+  }
+
+  customerUnsubscribed(id: any): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    return this.http
+      .get<any>(`http://localhost:3003/auth/unsubscribe/${id}`, { headers })
       .pipe(
         catchError((error) => {
           return throwError(error);
