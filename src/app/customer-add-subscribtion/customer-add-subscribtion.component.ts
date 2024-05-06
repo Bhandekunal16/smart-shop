7405cfbb4c0c8c7b602fbea02f2f1670d4709c7e
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-06 13:03:48 +0530

    "Added functionality to customer-add-subscribtion component and updated customer-view-subscribtion component"
    This diff includes changes to the customer-add-subscribtion and customer-view-subscribtion

--- src/app/customer-add-subscribtion/customer-add-subscribtion.component.ts
+++ src/app/customer-add-subscribtion/customer-add-subscribtion.component.ts
@@ -9,16 +9,24 @@ import { DecryptService } from '../../global/decrypt.service';
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+interface ApiResponse<T> {
+  status: boolean;
+  response: T;
+}
 
 @Component({
   selector: 'app-customer-add-subscribtion',
   standalone: true,
-  imports: [HttpClientModule, TableModule, ButtonModule],
+  imports: [HttpClientModule, TableModule, ButtonModule, CommonModule],
   templateUrl: './customer-add-subscribtion.component.html',
   styleUrl: './customer-add-subscribtion.component.scss',
 })
 export class CustomerAddSubscriptionComponent implements OnInit {
   products!: any[];
+  Add: any[] = [];
+
   constructor(
     private http: HttpClient,
     private decrypt: DecryptService,
@@ -26,59 +34,73 @@ export class CustomerAddSubscriptionComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.shopDetails().subscribe((ele) => {
+    const subscribedShop: any[] = [];
+
+    this.added().subscribe((ele: ApiResponse<any>) => {
       const data = this.decrypt.decrypt(ele.response);
-      this.products = data.data;
-      console.log(data);
+      this.Add = data.data;
+      subscribedShop.push(...data.data);
+    });
+
+    this.shopDetails().subscribe((ele: ApiResponse<any>) => {
+      const data = this.decrypt.decrypt(ele.response);
+      const firstArray = data.data;
+      this.newKey(subscribedShop, firstArray);
     });
   }
 
-  shopDetails(): Observable<any> {
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-    });
-    return this.http
-      .get<any>(`http://localhost:3003/shop/shops`, { headers })
-      .pipe(
-        catchError((error) => {
-          return throwError(error);
-        })
+  newKey(array1: any[], array2: any[]): void {
+    array1.forEach((obj1) => {
+      const matchedObj = array2.find(
+        (obj2: { id: any }) => obj1.id === obj2.id
       );
+      obj1.isSubscribed = !!matchedObj;
+    });
+    this.products = array1;
+    console.log(this.products[0].isSubscribed);
   }
 
-  subscribe(id: any) {
-    console.log(id);
+  added(): Observable<ApiResponse<any>> {
+    const id = localStorage.getItem('id');
+    const headers = this.getHeaders();
+    return this.http.get<ApiResponse<any>>(
+      `http://localhost:3003/auth/getAll/shop/subscribed/${id}`,
+      { headers }
+    );
+  }
 
-    this.Subscribe(id).subscribe((ele) => {
+  shopDetails(): Observable<ApiResponse<any>> {
+    const headers = this.getHeaders();
+    return this.http.get<ApiResponse<any>>(`http://localhost:3003/shop/shops`, {
+      headers,
+    });
+  }
+
+  subscribe(id: any): void {
+    this.Subscribe(id).subscribe((ele: ApiResponse<any>) => {
       const data = this.decrypt.decrypt(ele.response);
       data.status ? this.viewSubscriptionRoute() : console.log('nothing');
     });
   }
 
-  viewSubscriptionRoute() {
+  viewSubscriptionRoute(): void {
     this.router.navigate(['customer-dashboard/viewSubscription']);
   }
 
-  Subscribe(id: any): Observable<any> {
-    const headers = new HttpHeaders({
+  Subscribe(id: any): Observable<ApiResponse<any>> {
+    const headers = this.getHeaders();
+    const CustomerId = localStorage.getItem('id');
+    const body = { id, customerId: CustomerId };
+    return this.http.post<ApiResponse<any>>(
+      'http://localhost:3003/auth/customer/subscribe',
+      body,
+      { headers }
+    );
+  }
+
+  private getHeaders(): HttpHeaders {
+    return new HttpHeaders({
       'Content-Type': 'application/json',
     });
-
-    const CustomerId = localStorage.getItem('id');
-
-    const body = {
-      id: id,
-      customerId: CustomerId,
-    };
-
-    console.log(body);
-
-    return this.http
-      .post<any>('http://localhost:3003/auth/customer/subscribe', body, { headers })
-      .pipe(
-        catchError((error) => {
-          return throwError(error);
-        })
-      );
   }
 }
--- src/app/customer-view-subscribtion/customer-view-subscribtion.component.ts
+++ src/app/customer-view-subscribtion/customer-view-subscribtion.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  HttpClient,
+  HttpClientModule,
+  HttpHeaders,
+} from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
+import { DecryptService } from '../../global/decrypt.service';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-customer-view-subscribtion',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, TableModule, ButtonModule],
   templateUrl: './customer-view-subscribtion.component.html',
-  styleUrl: './customer-view-subscribtion.component.scss'
+  styleUrl: './customer-view-subscribtion.component.scss',
 })
-export class CustomerViewSubscriptionComponent {
+export class CustomerViewSubscriptionComponent implements OnInit {
+  products!: any[];
+  constructor(
+    private http: HttpClient,
+    private decrypt: DecryptService,
+    private router: Router
+  ) {}
 
+  ngOnInit(): void {
+    this.shopDetails().subscribe((ele) => {
+      const data = this.decrypt.decrypt(ele.response);
+      this.products = data.data;
+      console.log(data);
+    });
+  }
+
+  shopDetails(): Observable<any> {
+    const id = localStorage.getItem('id');
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+    return this.http
+      .get<any>(`http://localhost:3003/auth/getAll/shop/subscribed/${id}`, {
+        headers,
+      })
+      .pipe(
+        catchError((error) => {
+          return throwError(error);
+        })
+      );
+  }
 }
