6adad16c20da9ac5a2024c643e1ae2c7092a61b3
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-07 10:07:05 +0530

    "Add decryption service to feedback component and update constructor; add sendOtp method to send feedback using HttpClient"
    This diff adds the DecryptService to the FeedbackComponent's constructor, updates the constructor to include HttpClient

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -9,6 +9,9 @@ import {
   HttpHeaders,
 } from '@angular/common/http';
 import { ButtonModule } from 'primeng/button';
+import { Observable } from 'rxjs/internal/Observable';
+import { catchError, throwError } from 'rxjs';
+import { DecryptService } from '../../global/decrypt.service';
 
 @Component({
   selector: 'app-feedback',
@@ -27,7 +30,7 @@ export class FeedbackComponent {
   public myForm: FormGroup;
   public msg: Message[] | any;
 
-  constructor() {
+  constructor(private http: HttpClient, private decrypt: DecryptService) {
     this.myForm = new FormGroup({
       email: new FormControl(''),
       message: new FormControl(''),
@@ -38,6 +41,45 @@ export class FeedbackComponent {
     const email = this.myForm.value.email;
     const message = this.myForm.value.message;
 
+    const body = { email, message };
     console.log(email, message);
+
+    this.sendOtp(body).subscribe((ele) => {
+      const data = this.decrypt.decrypt(ele.response);
+
+      data.status
+        ? (this.msg = [
+            {
+              severity: 'success',
+              summary: 'success',
+              detail: `${data.data}`,
+            },
+          ])
+        : (this.msg = [
+            {
+              severity: 'warn',
+              summary: 'warn',
+              detail: `${data.data}`,
+            },
+          ]);
+
+      setInterval(() => {
+        window.location.reload();
+      }, 3000);
+    });
+  }
+
+  sendOtp(body: any): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    return this.http
+      .post<any>('http://localhost:3003/auth/feedback', body, { headers })
+      .pipe(
+        catchError((error) => {
+          return throwError(error);
+        })
+      );
   }
 }
