eedbc6e6ee058df19885b098a2b4674e5d130950
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-08 17:57:59 +0530

    Update customer-view-product, user-add-wishlist, and user-view-wishlist components
    * customer-view-product: change button label to "Rate Product"
    * user-add-wishlist

--- src/app/user-add-wishlist/user-add-wishlist.component.ts
+++ src/app/user-add-wishlist/user-add-wishlist.component.ts
@@ -1,12 +1,114 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  HttpClient,
+  HttpClientModule,
+  HttpHeaders,
+} from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
+import { CardModule } from 'primeng/card';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { Router } from '@angular/router';
+import { DecryptService } from '../../global/decrypt.service';
+import { RatingModule } from 'primeng/rating';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-add-wishlist',
   standalone: true,
-  imports: [],
+  imports: [
+    HttpClientModule,
+    CardModule,
+    CommonModule,
+    ButtonModule,
+    RatingModule,
+    FormsModule,
+  ],
   templateUrl: './user-add-wishlist.component.html',
-  styleUrl: './user-add-wishlist.component.scss'
+  styleUrl: './user-add-wishlist.component.scss',
 })
-export class UserAddWishlistComponent {
+export class UserAddWishlistComponent implements OnInit {
+  public data: any[] = [];
+  public value!: number;
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private decrypt: DecryptService
+  ) {}
 
+  ngOnInit(): void {
+    this.shopDetails().subscribe((ele) => {
+      const res = this.decrypt.decrypt(ele.response);
+
+      const updatedShops = res.data.map((shop: any) => {
+        const updatedProducts = shop.products.map((product: any) => ({
+          ...product,
+          shopName: shop.shopName,
+        }));
+
+        return {
+          products: updatedProducts,
+        };
+      });
+
+      let array = [];
+      for (let index = 0; index < 1; index++) {
+        const product = updatedShops;
+
+        for (let index = 0; index < product.length; index++) {
+          const element = product[index].products;
+          array.push(element);
+        }
+      }
+
+      const data = array.reduce((acc, arr) => [...acc, ...arr], []);
+      // console.log(data);
+
+      this.data = data;
+    });
+  }
+
+  setCurrentObjectId(id: string) {
+    console.log(id);
+    const userId = localStorage.getItem('id');
+    const body = {
+      userId: userId,
+      productId: id,
+    };
+    // localStorage.setItem('currentObjectId', id);
+    this.Add(body).subscribe((ele) => {
+      const res = this.decrypt.decrypt(ele.response);
+      console.log(res);
+    });
+  }
+
+  getStatusText(isPurchased: boolean): string {
+    return isPurchased ? 'Sold' : 'Unsold';
+  }
+
+  shopDetails(): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+    return this.http
+      .get<any>(`http://localhost:3003/product/customer/get`, { headers })
+      .pipe(
+        catchError((error) => {
+          return throwError(error);
+        })
+      );
+  }
+
+  Add(id: any): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+    return this.http
+      .post<any>(`http://localhost:3003/product/wishlist`, id, { headers })
+      .pipe(
+        catchError((error) => {
+          return throwError(error);
+        })
+      );
+  }
 }
--- src/app/user-view-wishlist/user-view-wishlist.component.ts
+++ src/app/user-view-wishlist/user-view-wishlist.component.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  HttpClient,
+  HttpClientModule,
+  HttpHeaders,
+} from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
+import { DecryptService } from '../../global/decrypt.service';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-view-wishlist',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HttpClientModule, ButtonModule, TableModule],
   templateUrl: './user-view-wishlist.component.html',
-  styleUrl: './user-view-wishlist.component.scss'
+  styleUrl: './user-view-wishlist.component.scss',
 })
 export class UserViewWishlistComponent {
+  constructor(
+    private http: HttpClient,
+    private decrypt: DecryptService,
+    private router: Router
+  ) {}
+  products!: any[];
 
+  ngOnInit(): void {
+    this.details();
+  }
+
+  details() {
+    const id = localStorage.getItem('id');
+    this.shopDetails(id).subscribe((ele) => {
+      console.log(ele);
+      const data = this.decrypt.decrypt(ele.response);
+      console.log(data.data);
+      this.products = data.data;
+    });
+  }
+
+  submit() {
+    this.add();
+  }
+
+  add(): void {
+    this.router.navigate(['customer-dashboard/userAddWishList']);
+  }
+
+  shopDetails(id: any): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    return this.http
+      .get<any>(`http://localhost:3003/product/get/wishlist/${id}`, { headers })
+      .pipe(
+        catchError((error) => {
+          return throwError(error);
+        })
+      );
+  }
 }
