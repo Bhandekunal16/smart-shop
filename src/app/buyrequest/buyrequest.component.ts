dfcdeed652b9a8ce80f0b96808647dc5a4966f2d
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-17 09:23:54 +0530

    "Added payment option dropdown to buy request component, implemented form control and validation, and created onPay and onselect methods"

--- src/app/buyrequest/buyrequest.component.ts
+++ src/app/buyrequest/buyrequest.component.ts
@@ -12,6 +12,12 @@ import { ButtonModule } from 'primeng/button';
 import { MessagesModule } from 'primeng/messages';
 import { Message } from 'primeng/api';
 import { Router } from '@angular/router';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-buyrequest',
@@ -22,18 +28,26 @@ import { Router } from '@angular/router';
     CommonModule,
     ButtonModule,
     MessagesModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './buyrequest.component.html',
   styleUrl: './buyrequest.component.scss',
 })
 export class BuyRequestComponent {
   products!: any[];
+  public options: string[] | any = ['CASH'];
   public msg: Message[] | any;
+  public myForm: FormGroup | any;
+  public mode: any;
   constructor(
     private http: HttpClient,
     private decrypt: DecryptService,
     private route: Router
-  ) {}
+  ) {
+    this.myForm = new FormGroup({
+      transactionType: new FormControl('', [Validators.required]),
+    });
+  }
 
   ngOnInit(): void {
     this.list();
@@ -50,11 +64,32 @@ export class BuyRequestComponent {
   }
 
   getStatusInfo(isPurchased: boolean): { text: string; class: string } {
+    console.log(isPurchased)
     if (isPurchased) {
       return { text: 'Purchased', class: 'purchased' };
     } else {
       return { text: 'Process', class: 'Process' };
     }
+  }
+
+  onPay(userId: any, productId: any) {
+    console.log(userId, productId);
+    const payload = {
+      custId: userId,
+      productId: productId,
+      transactionType: this.mode,
+    };
+    console.log(payload);
+
+    this.sell(payload).subscribe((ele)=> {
+      const data = this.decrypt.decrypt(ele.response);
+      console.log(data.data)
+    })
+  }
+
+  onselect(mode: any) {
+    console.log(mode);
+    this.mode = mode;
   }
 
   purchasedList(id: any): Observable<any> {
@@ -63,7 +98,23 @@ export class BuyRequestComponent {
     });
 
     return this.http
-      .get<any>(`http://localhost:3003/payment/purchase/request/${id}`, { headers })
+      .get<any>(`http://localhost:3003/payment/purchase/request/${id}`, {
+        headers,
+      })
+      .pipe(
+        catchError((error) => {
+          return throwError(error);
+        })
+      );
+  }
+
+  sell(body: any): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    return this.http
+      .post<any>('http://localhost:3003/payment/transaction', body, { headers })
       .pipe(
         catchError((error) => {
           return throwError(error);
