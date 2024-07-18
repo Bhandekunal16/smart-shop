edcae3e64cdbadacf118decc1a088b1ab0751e5e
bhandekunal16 <bhandekunal16@gmail.com>
2024-07-18 15:49:53 +0530

    "Updated CSS for button-grid in customer-view-product component and modified disable-shop component to add try-catch block and refactor code for fetching shop details"

--- src/app/disable-shop/disable-shop.component.ts
+++ src/app/disable-shop/disable-shop.component.ts
@@ -25,21 +25,27 @@ export class DisableShopComponent implements OnInit {
   }
 
   details() {
-    const id = localStorage.getItem('id');
-
-    this.msg = [{ severity: 'info', detail: 'searching for the shop details' }];
-
-    this.shopDetails({ id }).subscribe((ele) => {
-      const data = this.decryptService.decrypt(ele.response);
-      this.products.push(data.data);
+    try {
+      const id = localStorage.getItem('id');
 
       this.msg = [
-        {
-          severity: 'success',
-          detail: `shop details fetched for the ${data.data.shopName}`,
-        },
+        { severity: 'info', detail: 'searching for the shop details' },
       ];
-    });
+
+      this.shopDetails({ id }).subscribe((ele) => {
+        const data = this.decryptService.decrypt(ele.response);
+        this.products.push(data.data);
+
+        this.msg = [
+          {
+            severity: 'success',
+            detail: `shop details fetched for the ${data.data.shopName}`,
+          },
+        ];
+      });
+    } catch (error) {
+      console.log('some thing found with localhost');
+    }
   }
 
   shopDetails(body: any): Observable<any> {
