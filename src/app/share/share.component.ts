e741898029cbb5a521c67382c8a0b39bc97d2cf8
bhandekunal16 <bhandekunal16@gmail.com>
2024-07-26 13:01:14 +0530

    "Added feedback form to share component with Primeng UI, form validation, and API call to send email"

--- src/app/share/share.component.ts
+++ src/app/share/share.component.ts
@@ -1,12 +1,80 @@
 import { Component } from '@angular/core';
+import { Message } from 'primeng/api';
+import { FormGroup, FormControl } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { catchError, throwError } from 'rxjs';
+import { DecryptService } from '../../global/decrypt.service';
+import { SharedModule } from '../shared/shared.module';
 
 @Component({
   selector: 'app-share',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './share.component.html',
-  styleUrl: './share.component.scss'
+  styleUrl: './share.component.scss',
 })
 export class ShareComponent {
+  public myForm: FormGroup;
+  public msg: Message[] | any;
 
+  constructor(private http: HttpClient, private decrypt: DecryptService) {
+    this.myForm = new FormGroup({
+      email: new FormControl(''),
+    });
+  }
+
+  submitForm() {
+    const email = this.myForm.value.email;
+    const message = `
+    <h3>Hi,</h3>
+    <p>I found this interesting website and thought you might like it:</p>
+     
+    <p>https://cyborgcart.vercel.app/</p>
+    
+
+     <p>Best regards,</p>
+     <p>${email}</p>
+   `;
+
+    const body = { email, message };
+
+    this.sendOtp(body).subscribe((ele) => {
+      const data = this.decrypt.decrypt(ele.response);
+
+      data.status
+        ? (this.msg = [
+            {
+              severity: 'success',
+              summary: 'success',
+              detail: `${data.data}`,
+            },
+          ])
+        : (this.msg = [
+            {
+              severity: 'warn',
+              summary: 'warn',
+              detail: `${data.data}`,
+            },
+          ]);
+
+      this.myForm.reset();
+    });
+  }
+
+  sendOtp(body: any): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    return this.http
+      .post<any>('https://smart-shop-api-eta.vercel.app/auth/email', body, {
+        headers,
+      })
+      .pipe(
+        catchError((error) => {
+          return throwError(error);
+        })
+      );
+  }
 }
