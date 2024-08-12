9fbea06c150b463dfdd244d33972f6c5fce5bc99
bhandekunal16 <bhandekunal16@gmail.com>
2024-08-12 16:19:27 +0530

    "Added AddUrlComponent to merchant routes and imported it"

---
+++ src/app/add-url/add-url.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+import { Message } from 'primeng/api';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
+import { header } from '../string';
+import { DecryptService } from '../../global/decrypt.service';
+
+@Component({
+  selector: 'app-add-url',
+  standalone: true,
+  imports: [SharedModule],
+  templateUrl: './add-url.component.html',
+  styleUrl: './add-url.component.scss',
+})
+export class AddUrlComponent {
+  public value: string | undefined;
+  public msg: Message[] | any;
+
+  constructor(private http: HttpClient, private decrypt: DecryptService) {}
+
+  get() {
+    this.add({ id: localStorage.getItem('id'), url: this.value }).subscribe(
+      (ele) => {
+        const res = this.decrypt.decrypt(ele.response);
+      }
+    );
+    this.value = '';
+  }
+
+  public add(body: any): Observable<any> {
+    const headers = header();
+    return this.http
+      .post<any>('https://smart-shop-api-eta.vercel.app/shop/add/url', body, {
+        headers,
+      })
+      .pipe(
+        catchError((error) => {
+          return throwError(error);
+        })
+      );
+  }
+}
--- src/app/merchant.routes.ts
+++ src/app/merchant.routes.ts
@@ -1,23 +1,24 @@
-import { Routes } from "@angular/router";
-import { AddCustomerComponent } from "./add-customer/add-customer.component";
-import { AddProductComponent } from "./add-product/add-product.component";
-import { AddShopComponent } from "./add-shop/add-shop.component";
-import { BuyRequestComponent } from "./buyrequest/buyrequest.component";
-import { ChartProductComponent } from "./chart-product/chart-product.component";
-import { DeleteCustomerComponent } from "./delete-customer/delete-customer.component";
-import { EditShopComponent } from "./edit-shop/edit-shop.component";
-import { FeedComponent } from "./feed/feed.component";
-import { FeedbackComponent } from "./feedback/feedback.component";
-import { PaymentComponent } from "./payment/payment.component";
-import { PurchaseListComponent } from "./purchase-list/purchase-list.component";
-import { ShareComponent } from "./share/share.component";
-import { UpdateProductComponent } from "./update-product/update-product.component";
-import { UpdateProfileComponent } from "./update-profile/update-profile.component";
-import { UserAddWishlistComponent } from "./user-add-wishlist/user-add-wishlist.component";
-import { UserViewWishlistComponent } from "./user-view-wishlist/user-view-wishlist.component";
-import { UserlistComponent } from "./userlist/userlist.component";
-import { ViewProductComponent } from "./view-product/view-product.component";
-import { ViewShopComponent } from "./view-shop/view-shop.component";
+import { Routes } from '@angular/router';
+import { AddCustomerComponent } from './add-customer/add-customer.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { AddShopComponent } from './add-shop/add-shop.component';
+import { BuyRequestComponent } from './buyrequest/buyrequest.component';
+import { ChartProductComponent } from './chart-product/chart-product.component';
+import { DeleteCustomerComponent } from './delete-customer/delete-customer.component';
+import { EditShopComponent } from './edit-shop/edit-shop.component';
+import { FeedComponent } from './feed/feed.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { PaymentComponent } from './payment/payment.component';
+import { PurchaseListComponent } from './purchase-list/purchase-list.component';
+import { ShareComponent } from './share/share.component';
+import { UpdateProductComponent } from './update-product/update-product.component';
+import { UpdateProfileComponent } from './update-profile/update-profile.component';
+import { UserAddWishlistComponent } from './user-add-wishlist/user-add-wishlist.component';
+import { UserViewWishlistComponent } from './user-view-wishlist/user-view-wishlist.component';
+import { UserlistComponent } from './userlist/userlist.component';
+import { ViewProductComponent } from './view-product/view-product.component';
+import { ViewShopComponent } from './view-shop/view-shop.component';
+import { AddUrlComponent } from './add-url/add-url.component';
 
 export const Merchant: Routes = [
   {
@@ -45,6 +46,7 @@ export const Merchant: Routes = [
       { path: 'userAddWishList', component: UserAddWishlistComponent },
       { path: 'payment', component: PaymentComponent },
       { path: 'purchasedList', component: PurchaseListComponent },
+      { path: 'addUrls', component: AddUrlComponent },
     ],
   },
 ];
