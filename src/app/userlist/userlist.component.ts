f61cf2432549fc3a942f15da95d7ee82347488b7
bhandekunal16 <bhandekunal16@gmail.com>
2024-08-12 12:30:42 +0530

    "Refactor UserlistComponent: removed unused imports, simplified message handling, and extracted header creation into a separate method"

--- src/app/userlist/userlist.component.ts
+++ src/app/userlist/userlist.component.ts
@@ -3,9 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, catchError, throwError } from 'rxjs';
 import { DecryptService } from '../../global/decrypt.service';
 import { Message } from 'primeng/api';
-import { Router } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
-import { setInterval } from 'timers/promises';
 
 @Component({
   selector: 'app-userlist',
@@ -15,69 +13,39 @@ import { setInterval } from 'timers/promises';
   styleUrl: './userlist.component.scss',
 })
 export class UserlistComponent implements OnInit {
-  products!: any[];
+  public products!: any[];
   public msg: Message[] | any;
-  constructor(
-    private http: HttpClient,
-    private decrypt: DecryptService,
-    private route: Router
-  ) {}
+
+  constructor(private http: HttpClient, private decrypt: DecryptService) {}
 
   ngOnInit(): void {
     try {
-      this.msg = [
-        { severity: 'info', detail: 'searching for your customer...' },
-      ];
+      this.messageHandler('info', 'searching for your customer...');
       const id = localStorage.getItem('id');
       this.customerSubscribed(id).subscribe((ele) => {
         const data = this.decrypt.decrypt(ele.response);
-
-        data.status
-          ? (this.msg = [
-              { severity: 'success', detail: `user found ${data.data.length}` },
-            ])
-          : (this.msg = [{ severity: 'warn', detail: 'something went wrong' }]);
-
-        setTimeout(() => {
-          this.msg = [];
-        }, 100);
-
         this.products = data.data;
+        data.status
+          ? this.messageHandler('success', `user found ${data.data.length}`)
+          : this.messageHandler('warn', 'something went wrong');
+        this.clearMessagesAfterDelay();
       });
     } catch (error) {
-       ('issue of the localhost');
+      ('issue of the localhost');
     }
   }
 
-  unsubscribe(id: any) {
+  public unsubscribe(id: any) {
     this.customerUnsubscribed(id).subscribe((ele) => {
       const data = this.decrypt.decrypt(ele.response);
-
-      if (data.status) {
-        this.msg = [
-          {
-            severity: 'success',
-            summary: 'success',
-            detail: `${data.data}`,
-          },
-        ];
-      } else {
-        this.msg = [
-          {
-            severity: 'warn',
-            summary: 'warn',
-            detail: 'some addition failed.',
-          },
-        ];
-      }
+      data.status
+        ? this.messageHandler('success', `${data.data}`)
+        : this.messageHandler('warn', `${data.data}`);
     });
   }
 
-  customerSubscribed(id: any): Observable<any> {
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-    });
-
+  private customerSubscribed(id: any): Observable<any> {
+    const headers = this.header();
     return this.http
       .get<any>(
         `https://smart-shop-api-eta.vercel.app/auth/getAll/customers/subscribed/${id}`,
@@ -90,11 +58,8 @@ export class UserlistComponent implements OnInit {
       );
   }
 
-  customerUnsubscribed(id: any): Observable<any> {
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-    });
-
+  private customerUnsubscribed(id: any): Observable<any> {
+    const headers = this.header();
     return this.http
       .get<any>(
         `https://smart-shop-api-eta.vercel.app/auth/unsubscribe/${id}`,
@@ -105,5 +70,21 @@ export class UserlistComponent implements OnInit {
           return throwError(error);
         })
       );
+  }
+
+  private messageHandler(severity: string, detail: string, summary?: string) {
+    this.msg = [{ severity: severity, detail: detail, summary: summary }];
+  }
+
+  private header() {
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+  }
+
+  private clearMessagesAfterDelay() {
+    setTimeout(() => {
+      this.msg = [];
+    }, 1000);
   }
 }
