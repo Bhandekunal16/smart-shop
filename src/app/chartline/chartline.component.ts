16a3d97d8d1e8ebb8cd605cdc2460c1f6adc43e1
bhandekunal16 <bhandekunal16@gmail.com>
2024-08-30 11:23:58 +0530

    "Removed p-messages component and related code from chartline component"

--- src/app/chartline/chartline.component.ts
+++ src/app/chartline/chartline.component.ts
@@ -1,9 +1,5 @@
 import { Component, Input } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
-import { Observable, catchError, throwError } from 'rxjs';
-import { header } from '../string';
-import { HttpClient } from '@angular/common/http';
-import { Message } from 'primeng/api';
 
 @Component({
   selector: 'app-chartline',
@@ -13,11 +9,9 @@ import { Message } from 'primeng/api';
   styleUrl: './chartline.component.scss',
 })
 export class ChartlineComponent {
-  constructor(private http: HttpClient) {}
+  constructor() {}
   public data: any;
   public options: any;
-  public msg: Message[] | any;
-  public flag: boolean = false;
 
   @Input() header: undefined | string;
   @Input() data1: any[] | undefined;
@@ -25,14 +19,11 @@ export class ChartlineComponent {
 
   ngOnInit() {
     this.simulate();
-    this.flag = true;
   }
 
   private simulate() {
     const documentStyle = getComputedStyle(document.documentElement);
     const textColor = documentStyle.getPropertyValue('--text-color');
-
-    console.log(this.data1, this.data2)
 
     this.data = {
       labels: this.data1,
@@ -63,37 +54,5 @@ export class ChartlineComponent {
         },
       },
     };
-
-    // setTimeout(() => {
-    //   this.data = {
-    //     labels: this.data1,
-    //     datasets: [
-    //       {
-    //         data: this.data2,
-    //         backgroundColor: [
-    //           documentStyle.getPropertyValue('--blue-500'),
-    //           documentStyle.getPropertyValue('--yellow-500'),
-    //           documentStyle.getPropertyValue('--green-500'),
-    //         ],
-    //         hoverBackgroundColor: [
-    //           documentStyle.getPropertyValue('--blue-400'),
-    //           documentStyle.getPropertyValue('--yellow-400'),
-    //           documentStyle.getPropertyValue('--green-400'),
-    //         ],
-    //       },
-    //     ],
-    //   };
-
-    //   this.options = {
-    //     cutout: '60%',
-    //     plugins: {
-    //       legend: {
-    //         labels: {
-    //           color: textColor,
-    //         },
-    //       },
-    //     },
-    //   };
-    // }, 3000);
   }
 }
