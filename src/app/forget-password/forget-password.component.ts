f75696dca56a9215d83b0c13d19daa5505caeaa1
bhandekunal16 <bhandekunal16@gmail.com>
2024-09-02 16:58:54 +0530

    "Added OTP verification dialog to forget password component"

--- src/app/forget-password/forget-password.component.ts
+++ src/app/forget-password/forget-password.component.ts
@@ -18,10 +18,16 @@ export class ForgetPasswordComponent {
   public myForm: FormGroup;
   public msg: Message[] | any;
   public flag: boolean = true;
+  public visible: boolean = false;
+  public myForm2: FormGroup;
 
   constructor(private router: Router, private http: HttpClient) {
     this.myForm = new FormGroup({
       email: new FormControl(''),
+    });
+
+    this.myForm2 = new FormGroup({
+      otp: new FormControl(''),
     });
   }
 
@@ -36,9 +42,40 @@ export class ForgetPasswordComponent {
         this.messageHandler('warn', 'something went wrong');
       } else {
         this.messageHandler('success', 'otp send to your email.');
-        this.verifyOtp();
+        // this.verifyOtp();
+        this.showDialog();
       }
     });
+  }
+
+  public submitForm2() {
+    const otp: string = this.myForm2.value.otp;
+    const email: string | any = localStorage.getItem('email');
+    this.flag = false;
+    this.messageHandler('info', 'checking otp!');
+    this.verifyOtp2({ otp, email }).subscribe((data) => {
+      this.flag = true;
+      if (data.status) {
+        this.messageHandler('success', 'otp-verified');
+        this.conformPassword();
+      } else this.messageHandler('warn', `${data.msg}`);
+      this.clearMessagesAfterDelay();
+    });
+  }
+
+  private verifyOtp2(body: any): Observable<any> {
+    const headers = header();
+    return this.http
+      .post<any>(
+        'https://smart-shop-api-eta.vercel.app/auth/otp/verify',
+        body,
+        { headers }
+      )
+      .pipe(
+        catchError((error) => {
+          return throwError(error);
+        })
+      );
   }
 
   private sendOtp(body: any): Observable<any> {
@@ -58,7 +95,17 @@ export class ForgetPasswordComponent {
     this.msg = [{ severity: severity, detail: detail, summary: summary }];
   }
 
-  private verifyOtp(): void {
-    this.router.navigate(['/verify-otp']);
+  public showDialog() {
+    this.visible = true;
+  }
+
+  private clearMessagesAfterDelay() {
+    setTimeout(() => {
+      this.msg = [];
+    }, 1000);
+  }
+
+  public conformPassword(): void {
+    this.router.navigate(['/conform-password']);
   }
 }
