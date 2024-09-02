1202250bba1ea69b4aecc06cc8aeca00f73b8dac
bhandekunal16 <bhandekunal16@gmail.com>
2024-09-02 17:01:24 +0530

    "Removed VerifyOtpComponent and its associated files (HTML, SCSS, spec.ts) and removed route from app.routes.ts"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ForgetPasswordComponent } from './forget-password/forget-password.component';
-import { VerifyOtpComponent } from './verify-otp/verify-otp.component';
 import { ConformPasswordComponent } from './conform-password/conform-password.component';
 import { CustomerDashboardComponent } from './customer-dashboard/customer-dashboard.component';
 import { ShareProfileComponent } from './share-profile/share-profile.component';
@@ -12,18 +11,19 @@ export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'forget-password', component: ForgetPasswordComponent },
-  { path: 'verify-otp', component: VerifyOtpComponent },
   { path: 'conform-password', component: ConformPasswordComponent },
   { path: 'in/:id', component: ShareProfileComponent },
   {
     path: 'dashboard',
     component: DashboardComponent,
-    loadChildren: () => import('./merchant.routes').then((mod) => mod.MerchantRoutes),
+    loadChildren: () =>
+      import('./merchant.routes').then((mod) => mod.MerchantRoutes),
   },
   {
     path: 'customer-dashboard',
     component: CustomerDashboardComponent,
-    loadChildren: () => import('./customer.routes').then((mod) => mod.CustomerRoutes),
+    loadChildren: () =>
+      import('./customer.routes').then((mod) => mod.CustomerRoutes),
   },
   { path: '**', redirectTo: '' },
 ];
