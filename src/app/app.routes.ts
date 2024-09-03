9102f8c2776b64bb11222b8773fdaf9854bdc61d
bhandekunal16 <bhandekunal16@gmail.com>
2024-09-03 10:39:24 +0530

    "Removed ConformPasswordComponent and its associated files (HTML, SCSS, spec.ts) from the app."

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ForgetPasswordComponent } from './forget-password/forget-password.component';
-import { ConformPasswordComponent } from './conform-password/conform-password.component';
 import { CustomerDashboardComponent } from './customer-dashboard/customer-dashboard.component';
 import { ShareProfileComponent } from './share-profile/share-profile.component';
 
@@ -11,7 +10,6 @@ export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'forget-password', component: ForgetPasswordComponent },
-  { path: 'conform-password', component: ConformPasswordComponent },
   { path: 'in/:id', component: ShareProfileComponent },
   {
     path: 'dashboard',
